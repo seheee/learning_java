532c5302bdd917a1e18986308f2c0a7f178b5069
seheee <4554010@naver.com>
2021-01-09 19:25:39 +0900

    array

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -70,5 +70,52 @@ public class Main {
             }
         }
         System.out.println("end");
+
+
+        /* array */
+        int[] nArray;
+        nArray = new int[5];
+        for(i=0; i<5; i++) {
+            nArray[i] = i + 1;
+            System.out.printf("%1$d ", nArray[i]);
+        }
+        System.out.println();
+
+        for(i=0; i<nArray.length; i++) {
+            System.out.printf("%1$d ", nArray[i]);
+        }
+        System.out.println();
+        // default : int(0), char(null), float(0.0), boolean(false)
+
+        int[] arr = {1, 2, 3}; // 객체 생성됨
+        for(i=0; i<arr.length; i++) {
+            System.out.printf("%1$d ", arr[i]);
+        }
+        System.out.println();
+
+        int var = 1;
+        int[][] arr2;
+        arr2 = new int[3][];
+        arr2[0] = new int[2];
+        arr2[1] = new int[3];
+        arr2[2] = new int[4];
+
+        for(i=0; i<arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                arr2[i][j] = var++;
+                System.out.print(arr2[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        int[][] nArray1 = {{1, 2}, {3, 4}};
+        int[][] nArray2 = {{1, 2}, {3, 4, 5, 6}};
+        for(i=0; i<nArray2.length; i++) {
+            for (int j = 0; j < nArray2[i].length; j++) {
+                System.out.print(nArray2[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
